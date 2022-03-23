a97c150162d47bed35bda57f8576dd6ec98facd4
AleksMajerowicz <amajerowicz222@gmail.com>
2022-03-23 11:49:59 +0100

    Comment program Liczby Pierwsze and NWD i NWW

--- C++/NWD i NWW/NWD i NWW.cpp
+++ C++/NWD i NWW/NWD i NWW.cpp
@@ -7,32 +7,33 @@ using namespace std;
 
 int main()
 {
-    int a, b, c, NWW;
+    int a, b, c, NWW;//Tworzenie zmiennych: a-zmienna na I liczbę, b- II liczbę, c-Iloczyn a i b w celu niepowtarzania niepotrzebnie wyliczania NWD(poniewaz operujemy na czysto na a i b),do wyliczenia NWW i zmienna NWW-przechowujaca NWW
 
     cout << "Podaj Pierwszą Liczbę: ";
-    cin >> a;
+    cin >> a;//Podawanie I liczby
 
     cout << "Podaj Drugą Liczbę: ";
-    cin >> b;
+    cin >> b;//Podawanie II liczby
 
-    c = a * b;
+    c = a * b;//Iloczny a i b
 
-    while (a != b)
+    while (a != b)//Pętla ta,będzie się powtarzać doputy,doputy a i b nie bedą sobie równe.To pozwala,w pętli na zmiejszanie liczby a i b, żeby po przez ich różnicę,znaleśc NWD
     {
-        if (a < b)
+        if (a < b)//Jeżeli a jest mniejsze od b
         {
-            b = b - a;
+            b = b - a;//To od b odejmójemy od a
         }
-        else if (b < a)
+        else if (b < a)//a jezeli b jest mniejsze od a
         {
-            a = a - b;
+            a = a - b;//To od a odejmójemy b
         }
+        //Jest to po to,by zmiejszać liczby a i b tak,żeby je ze sobą porównać
     }
 
-    NWW = c / a;
+    NWW = c / a;//Dzielenie iloczynu a i b,przez NWD(mogłoby być też b,bo a i b,to to samo,czyli oba są NWD)
 
-    cout << "Największy Wspólny Dzielnik: " << a << endl;
-    cout << "Największa Wspólna Wielokrotność: " << NWW;
+    cout << "Największy Wspólny Dzielnik: " << a << endl;//Wyswieltanie NWD
+    cout << "Największa Wspólna Wielokrotność: " << NWW;//Wyświeltanie NWW
 }
 
 // Uruchomienie programu: Ctrl + F5 lub menu Debugowanie > Uruchom bez debugowania
