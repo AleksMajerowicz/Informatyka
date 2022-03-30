7a89bb645c13359ea938107eae2d0e0e0931d8dc
AleksMajerowicz <amajerowicz222@gmail.com>
2022-03-30 11:58:50 +0200

    Added Szyfrowanie kodme ASCII

---
+++ C++/Szyfrowanie kodem ASCII/Szyfrowanie kodem ASCII.cpp
@@ -0,0 +1,43 @@
+﻿// Szyfrowanie kodem ASCII.cpp : Ten plik zawiera funkcję „main”. W nim rozpoczyna się i kończy wykonywanie programu.
+//
+
+using namespace std;
+
+#include <iostream>
+
+void SzyfrowanieIDeszyfrowanie(char slowo[], int k)
+{
+    if (!(k >= -26 && k <= 26))
+    {
+        int d = strlen(slowo);
+        if (k >= 0)
+        {
+
+        }
+    }
+}
+
+int main()
+{
+    char tab[200];
+    int klucz;
+
+    cout << "Podaj słowo z małych Liter!: ";
+    cin >> tab;
+
+    cout << "Podaj klucz: ";
+    cin >> klucz;
+
+
+}
+
+// Uruchomienie programu: Ctrl + F5 lub menu Debugowanie > Uruchom bez debugowania
+// Debugowanie programu: F5 lub menu Debugowanie > Rozpocznij debugowanie
+
+// Porady dotyczące rozpoczynania pracy:
+//   1. Użyj okna Eksploratora rozwiązań, aby dodać pliki i zarządzać nimi
+//   2. Użyj okna programu Team Explorer, aby nawiązać połączenie z kontrolą źródła
+//   3. Użyj okna Dane wyjściowe, aby sprawdzić dane wyjściowe kompilacji i inne komunikaty
+//   4. Użyj okna Lista błędów, aby zobaczyć błędy
+//   5. Wybierz pozycję Projekt > Dodaj nowy element, aby utworzyć nowe pliki kodu, lub wybierz pozycję Projekt > Dodaj istniejący element, aby dodać istniejące pliku kodu do projektu
+//   6. Aby w przyszłości ponownie otworzyć ten projekt, przejdź do pozycji Plik > Otwórz > Projekt i wybierz plik sln
